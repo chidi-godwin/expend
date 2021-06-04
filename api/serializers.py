a1bbbbbcb38e3fad2859ac7e1ab9214aa90c10c7
chidi-godwin <chidieberen1998@gmail.com>
2021-06-04 08:33:15 +0100

    update user endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'email']
 
 
 class BankStatementSerializer(serializers.ModelSerializer):
